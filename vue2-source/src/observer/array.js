2d92917b8be2885df27c6c8283b63b47f4f4e385
rubine413 <rubine.lee@qq.com>
2021-03-13 06:50:58 +0800

    '数组劫持'

--- vue2-source/src/observer/array.js
+++ vue2-source/src/observer/array.js
@@ -8,7 +8,18 @@ methods.forEach(method => {
   ArrayMethods[method] = function (...args) {
     console.log(`数组劫持 ${method} -> ${args}`);
     const result = oldArrayProtoMethods[method].apply(this, args);
-
+    let inserted = null;
+    switch (method) {
+      case 'push':
+      case 'unshift':
+        inserted = args
+        break;
+      case 'splice':
+        inserted = args.slice(2)
+      default:
+        break;
+    }
+    console.log(`数据劫持 ${method} -> ${inserted}`)
     return result;
   };
 });
